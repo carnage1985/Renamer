a28abdd1fa6d99b755ec52930163f75493ba6f60
carnage1985 <carnage85@gmail.com>
2023-07-14 13:04:27 +0200

    voiceStateUpdate working
    User wird erkannt, wenn er Channel joined und Nickname ist bereit. Es fehlt die Methode um den Nicknamen zu ändern.

--- bot.js
+++ bot.js
@@ -9,19 +9,46 @@ const client = new Client({
 	],
 });
 const token = 'MTEyOTMxMTgwNjk4NTM1OTQ1MQ.GNBNuj.vgTaUkabn-78zzU3UO32e_zpEHkffPH3SZwU_o';
-const targetUserId = '324155395709075457'; // ID des Benutzers, auf den du reagieren möchtest
-const targetVoiceChannelId = '1123167904947507210'; // ID des Sprachkanals, auf den du reagieren möchtest
+const channelMappings = {
+  '1123167904947507210': { //Darker
+    '219790807454973952': 'Halvar Saluk',
+    '469913673142501376': 'Lyra',
+    '603651594503979008': 'Moradrosch',
+    '1118451880998228048': 'Oriphan',
+	'324155395709075457': 'DM',	
+  },
+  '1123167792309477417': { //Strahd
+    '219790807454973952': 'Alvin Stopperdukk',
+    '249208306470486016': 'Arkino',
+    '452762791326253056': 'Sir Samuel Ramkin',
+    '219801430158999552': 'Theodora',
+	'324155395709075457': 'DM',
+  }
+};
 
 client.on('ready', () => {
   console.log('Client ist online!');
 });
 
-client.on('voiceStateUpdate', (oldMember, newMember) => {
-	let newUserChannel = newMember.channelId
-	let oldUserChannel = oldMember.channelId
-	console.log('Neuer Channel:'+ newUserChannel);
-	console.log('Alter Channel:'+ oldUserChannel);
+client.on('voiceStateUpdate', (oldState, newState) => {
+
+	//Channel joinen
+	if (channelMappings[newState?.channelId]) {
+		console.log('Channel: '+ newState?.channelId);
+		
+		const channelEntry = channelMappings[newState?.channelId];
+		const nickname = channelEntry[newState?.id];
+		console.log('User: '+ nickname);
+		
+		//Nick ändern
+		if (channelEntry[newState?.id]) {
+			console.log('Benutzer ändern');
+			//kA wie ich den Nickname ändern
+		}
+	}
 });
 
 
 client.login(token);
+
+
