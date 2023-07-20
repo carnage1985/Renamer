e0ece2c8bd5659457ed74c3c72434c0446369c47
carnage1985 <carnage85@gmail.com>
2023-07-20 23:48:13 +0200

    loading json works

--- bot.js
+++ bot.js
@@ -1,4 +1,7 @@
 const { Client, GatewayIntentBits } = require('discord.js');
+const fs = require('fs');
+const path = require('path');
+
 const client = new Client({
 	intents: [
 		GatewayIntentBits.Guilds,
